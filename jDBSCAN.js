cf72d6738b63d5192528d303b31eedda7550e8ca
olezt <it21119@hua.gr>
2016-08-22 01:32:25 +0300

    Switch back to upphiminn data format
    Switch back to upphiminn data format for Geospatial data

--- jDBSCAN.js
+++ jDBSCAN.js
@@ -81,27 +81,29 @@
 		}
 
 		function haversine_distance(point1, point2) {
-                // default 4 sig figs reflects typical 0.3% accuracy of spherical model
-                if (typeof precision == 'undefined')
-                    precision = 4;
+			// default 4 sig figs reflects typical 0.3% accuracy of spherical model
+			if (typeof precision === 'undefined') {
+				var precision = 4;
+			}
 
-                var R = 6371;
-                var lat1 = point1.lat * Math.PI / 180,
-                        lon1 = point1.lon * Math.PI / 180;
-                var lat2 = point2.lat * Math.PI / 180,
-                        lon2 = point2.lon * Math.PI / 180;
+			var R = 6371;
+			var lat1 = point1.location.latitude * Math.PI / 180,
+				lon1 = point1.location.longitude * Math.PI / 180;
+			var lat2 = point2.location.latitude * Math.PI / 180,
+				lon2 = point2.location.longitude * Math.PI / 180;
 
-                var dLat = lat2 - lat1;
-                var dLon = lon2 - lon1;
+			var dLat = lat2 - lat1;
+			var dLon = lon2 - lon1;
 
-                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                        Math.cos(lat1) * Math.cos(lat2) *
-                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-                var d = R * c;
+			var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+				Math.cos(lat1) * Math.cos(lat2) *
+				Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
-                return d.toPrecision(precision);
-            }
+			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+			var d = R * c;
+
+			return d.toPrecision(precision);
+		}
 
 		//Core Algorithm Related
 		function get_region_neighbours(point_idx) {
@@ -242,18 +244,19 @@ function set_neighbour(neighbour) {
 				}
 
 				if (distance === haversine_distance) {
-					clusters_centers[i] = {lat: 0, lon: 0, accuracy: 0};
-                            for (var j = 0; j < clusters[i].length; j++) {
-                                clusters_centers[i].lat += +data[clusters[i][j]].lat;
-                                clusters_centers[i].lon += +data[clusters[i][j]].lon;
-                                clusters_centers[i].accuracy += +data[clusters[i][j]].accuracy;
-                            }
+					clusters_centers[i] = {location: {latitude: 0, longitude: 0, accuracy: 0}};
+
+					for (var j = 0; j < clusters[i].length; j++) {
+						clusters_centers[i].location.latitude += data[clusters[i][j]].location.latitude;
+						clusters_centers[i].location.longitude += data[clusters[i][j]].location.longitude;
+						clusters_centers[i].location.accuracy += data[clusters[i][j]].location.accuracy;
+					}
 
 					clusters_centers[i].dimension = clusters[i].length;
 					clusters_centers[i].parts = clusters[i];
-					clusters_centers[i].lat /= clusters[i].length;
-                                        clusters_centers[i].lon /= clusters[i].length;
-					clusters_centers[i].accuracy /= clusters[i].length;
+					clusters_centers[i].location.latitude /= clusters[i].length;
+					clusters_centers[i].location.longitude /= clusters[i].length;
+					clusters_centers[i].location.accuracy /= clusters[i].length;
 				}
 
 				if (time_eps) {
